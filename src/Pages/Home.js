250fdc3cb8989bc5a93f71f5167e4558bf1589cb
syedaCodes <syedaa.codes@gmail.com>
2021-06-11 17:32:10 +0530

    hero section in-progress

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import Sidebar from '../Components/Sidebar/index';
-import Navbar from '../Components/Navbar/index';
+import Sidebar from '../Components/Sidebar';
+import Navbar from '../Components/Navbar';
+import Hero from '../Components/HeroSection';
 
 const Home = () => {
     const [ isOpen, setIsOpen ] = useState(false);
@@ -13,6 +14,7 @@ const Home = () => {
         <>
             <Sidebar isOpen={isOpen} toggle={toggle}/>
             <Navbar toggle={toggle}/>
+            <Hero />
         </>
     );
 };
