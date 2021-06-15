36e114990488dbe02f883b9523cdce80e56f6575
syedaCodes <syedaa.codes@gmail.com>
2021-06-16 01:09:02 +0530

    added homepage to package.json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Signin from './Pages/Signin';
 import './index.css';
-import App from './App';
 
+const App = () => {
+
+    return (
+        <Router basename="/finch">
+            <Switch>
+                <Route path="/" component={Home} exact />
+                <Route path="/signin" component={Signin} exact />
+            </Switch>
+        </Router>
+    )
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
